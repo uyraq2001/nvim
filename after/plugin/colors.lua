9c1799c02b79a180e10863c30e2a54ff4b6bcfb3
Michael Chernigin <michaelchernigin@gmail.com>
2023-07-30 17:18:37 +0400

    Actually beautiful git gutter

--- after/plugin/colors.lua
+++ after/plugin/colors.lua
@@ -2,3 +2,4 @@ vim.cmd.colorscheme("ayu-mirage")
 
 vim.api.nvim_set_hl(0, "Normal", { bg = "none" })
 vim.api.nvim_set_hl(0, "NormalFloat", { bg = "none" })
+vim.api.nvim_set_hl(0, "SignColumn", { bg = "none" })
