b124e59e860828e322196583993d5cb4e65c160a
Michael Chernigin <michaelchernigin@gmail.com>
2023-11-20 16:12:10 +0400

    Try out LazyVim

--- after/plugin/colors.lua
+++
@@ -1,5 +0,0 @@
-vim.cmd.colorscheme("ayu-dark")
-
-vim.api.nvim_set_hl(0, "Normal", { bg = "none" })
-vim.api.nvim_set_hl(0, "NormalFloat", { bg = "none" })
-vim.api.nvim_set_hl(0, "SignColumn", { bg = "none" })
--- after/plugin/trim.lua
+++
@@ -1,3 +0,0 @@
-require("trim").setup({
-    trim_last_line = false,
-})
