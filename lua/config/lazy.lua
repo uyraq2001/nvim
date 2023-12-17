4906b718b2b758b808fa252f6ecd835a5a876ca2
Michael Chernigin <michaelchernigin@gmail.com>
2023-12-17 20:15:25 +0400

    chore: remove rust inline hints and funny plugin

--- lua/config/lazy.lua
+++ lua/config/lazy.lua
@@ -15,6 +15,10 @@ require("lazy").setup({
     { import = "lazyvim.plugins.extras.lang.clangd" },
     { import = "lazyvim.plugins.extras.lang.rust" },
     { import = "lazyvim.plugins.extras.lang.python" },
+    { import = "lazyvim.plugins.extras.lang.docker" },
+    { import = "lazyvim.plugins.extras.lang.json" },
+    { import = "lazyvim.plugins.extras.lang.yaml" },
+    { import = "lazyvim.plugins.extras.lang.markdown" },
     -- import/override with your plugins
     { import = "plugins" },
   },
