f5b55d3c0c1c3fa1b09f8709e8e94e4ef880c480
Michael Chernigin <chernigin@altlinux.org>
2024-02-07 13:33:00 +0400

    feat: switch to catppuccin

--- lua/plugins/colorscheme.lua
+++ lua/plugins/colorscheme.lua
@@ -1,9 +1,24 @@
 return {
-  "Shatur/neovim-ayu",
+  "catppuccin/nvim",
+  name = "catppuccin",
   lazy = false,
   priority = 1000,
   config = function()
-    require("ayu").setup({})
-    vim.cmd.colorscheme("ayu")
+    require("catppuccin").setup({
+      flavour = "macchiato",
+      transparent_background = true,
+      integrations = {
+        cmp = true,
+        gitsigns = true,
+        nvimtree = true,
+        treesitter = true,
+        notify = true,
+        mini = {
+          enabled = true,
+          indentscope_color = "",
+        },
+      },
+    })
+    vim.cmd.colorscheme("catppuccin")
   end,
 }
