ea157d6c3ba93fec8310f5e9faf59b13e64db764
uyraq2001 <uyraq2001@gmail.com>
2024-08-26 01:47:03 +0400

    feat: added spider

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -1,3 +1,36 @@
 -- Keymaps are automatically loaded on the VeryLazy event
 -- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
 -- Add any additional keymaps here
+
+vim.keymap.set(
+	{ "n", "o", "x" },
+	"w",
+	"<cmd>lua require('spider').motion('w')<CR>",
+	{ desc = "Spider-w" }
+)
+vim.keymap.set(
+	{ "n", "o", "x" },
+	"e",
+	"<cmd>lua require('spider').motion('e')<CR>",
+	{ desc = "Spider-e" }
+)
+vim.keymap.set(
+	{ "n", "o", "x" },
+	"b",
+	"<cmd>lua require('spider').motion('b')<CR>",
+	{ desc = "Spider-b" }
+)
+
+-- OR: lazy-load on keystroke
+-- lazy.nvim
+{
+	"chrisgrieser/nvim-spider",
+	keys = {
+		{
+			"e",
+			"<cmd>lua require('spider').motion('e')<CR>",
+			mode = { "n", "o", "x" },
+		},
+		-- ...
+	},
+},
