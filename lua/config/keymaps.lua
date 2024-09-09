ca223800dcf3c579a590b2eb24b2a0dad3a5e761
Kozyrev Yuri <kozyrevid@altlinux.org>
2024-09-09 09:56:18 +0400

    fix: fixed spider key maps

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -2,35 +2,6 @@
 -- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
 -- Add any additional keymaps here
 
-vim.keymap.set(
-	{ "n", "o", "x" },
-	"w",
-	"<cmd>lua require('spider').motion('w')<CR>",
-	{ desc = "Spider-w" }
-)
-vim.keymap.set(
-	{ "n", "o", "x" },
-	"e",
-	"<cmd>lua require('spider').motion('e')<CR>",
-	{ desc = "Spider-e" }
-)
-vim.keymap.set(
-	{ "n", "o", "x" },
-	"b",
-	"<cmd>lua require('spider').motion('b')<CR>",
-	{ desc = "Spider-b" }
-)
-
--- OR: lazy-load on keystroke
--- lazy.nvim
-{
-	"chrisgrieser/nvim-spider",
-	keys = {
-		{
-			"e",
-			"<cmd>lua require('spider').motion('e')<CR>",
-			mode = { "n", "o", "x" },
-		},
-		-- ...
-	},
-},
+vim.keymap.set({ "n", "o", "x" }, "w", "<cmd>lua require('spider').motion('w')<CR>", { desc = "Spider-w" })
+vim.keymap.set({ "n", "o", "x" }, "e", "<cmd>lua require('spider').motion('e')<CR>", { desc = "Spider-e" })
+vim.keymap.set({ "n", "o", "x" }, "b", "<cmd>lua require('spider').motion('b')<CR>", { desc = "Spider-b" })
