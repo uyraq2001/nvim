cbc3dcf4d876ae5d000c7399086040167b3da8bb
Kozyrev Yuri <kozyrevid@altlinux.org>
2024-09-09 09:52:37 +0400

    feat: disabled supermaven

--- lua/plugins/supermaven.lua
+++ lua/plugins/supermaven.lua
@@ -1,12 +1,12 @@
-return {
-  "supermaven-inc/supermaven-nvim",
-  config = function()
-    require("supermaven-nvim").setup({
-      keymaps = {
-        accept_suggestion = "<C-j>",
-        clear_suggestion = "<C-]>",
-        accept_word = "<C-S-j>",
-      },
-    })
-  end,
-}
+--return {
+--  "supermaven-inc/supermaven-nvim",
+--  config = function()
+--    require("supermaven-nvim").setup({
+--      keymaps = {
+--        accept_suggestion = "<C-j>",
+--        clear_suggestion = "<C-]>",
+--        accept_word = "<C-S-j>",
+--      },
+--    })
+--  end,
+--}
