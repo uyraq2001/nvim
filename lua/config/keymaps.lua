b9e52275a829adf74ab875056f7be3f307d33c9d
Kozyrev Yuri <kozyrevid@altlinux.org>
2024-09-10 18:59:40 +0400

    feat: added exchange

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -5,3 +5,8 @@
 vim.keymap.set({ "n", "o", "x" }, "w", "<cmd>lua require('spider').motion('w')<CR>", { desc = "Spider-w" })
 vim.keymap.set({ "n", "o", "x" }, "e", "<cmd>lua require('spider').motion('e')<CR>", { desc = "Spider-e" })
 vim.keymap.set({ "n", "o", "x" }, "b", "<cmd>lua require('spider').motion('b')<CR>", { desc = "Spider-b" })
+
+vim.keymap.set("n", "sx", require("substitute.exchange").operator, { noremap = true, desc = "Exchange" })
+vim.keymap.set("n", "sxx", require("substitute.exchange").line, { noremap = true, desc = "Exchange line" })
+vim.keymap.set("x", "X", require("substitute.exchange").visual, { noremap = true, desc = "Exchange" })
+vim.keymap.set("n", "sxc", require("substitute.exchange").cancel, { noremap = true, desc = "Cancel exchange" })
---
+++ lua/plugins/substitute.lua
@@ -0,0 +1,9 @@
+return {
+  "gbprod/substitute.nvim",
+  opts = {
+    -- your configuration comes here
+    -- or leave it empty to use the default settings
+    -- refer to the configuration section below
+    exchange = { use_esc_to_cancel = true },
+  },
+}
